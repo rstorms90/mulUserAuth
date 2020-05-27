4475d3de5c63de26bfa3e9ce89fb9a90a6e28174
Russ Storms <rstorms90@gmail.com>
2020-05-27 12:47:29 -0600

    change readme

--- server/src/isAuth.ts
+++ server/src/isAuth.ts
@@ -1,22 +1,22 @@
-import { MiddlewareFn } from 'type-graphql';
-import { verify } from 'jsonwebtoken';
-import { MyContext } from './MyContext';
+import { MiddlewareFn } from "type-graphql";
+import { verify } from "jsonwebtoken";
+import { MyContext } from "./MyContext";
 
 // Middleware for user protected routes
 export const isAuth: MiddlewareFn<MyContext> = ({ context }, next) => {
-  const authorization = context.req.headers['authorization'];
+  const authorization = context.req.headers["authorization"];
 
   if (!authorization) {
-    throw new Error('Unauthenticated');
+    throw new Error("Unauthenticated");
   }
 
   try {
-    const token = authorization.split(' ')[1];
+    const token = authorization.split(" ")[1];
     const payload = verify(token, process.env.ACCESS_TOKEN_SECRET!);
     context.payload = payload as any;
   } catch (err) {
     console.log(err);
-    throw new Error('Unauthenticated');
+    throw new Error("Unauthenticated");
   }
 
   return next();
